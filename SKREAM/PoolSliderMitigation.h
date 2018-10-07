4025caa908a8b997ceb8931ce194510e818fae55
SENTINELONE\yarden <yarden@sentinelone.com>
2018-10-07 19:47:41 +0300

    Use a single definition for POOL_HEADER.

--- SKREAM/PoolSliderMitigation.h
+++ SKREAM/PoolSliderMitigation.h
@@ -1,6 +1,43 @@
 #pragma once
 
 #include <ntifs.h>
+#include <minwindef.h>
+
+#pragma warning(disable : 4201)
+
+// POOL_HEADER struct taken from reactOs
+typedef struct _POOL_HEADER
+{
+    union
+    {
+        struct 
+        {
+            USHORT PreviousSize : 8;
+            USHORT PoolIndex : 8;
+            USHORT BlockSize : 8;
+            USHORT PoolType : 8;
+        };
+        ULONG Ulong1;
+    };
+#ifdef _M_AMD64
+    ULONG PoolTag;
+#endif
+    union
+    {
+#ifdef _M_AMD64
+        PEPROCESS ProcessBilled;
+#else
+        ULONG PoolTag;
+#endif
+        struct 
+        {
+            USHORT AllocatorBackTraceIndex;
+            USHORT PoolTagHash;
+        };
+    };
+} POOL_HEADER, *PPOOL_HEADER;
+
+#pragma warning(default : 4201)
 
 VOID
 PoolSliderLoadImageNotify(
