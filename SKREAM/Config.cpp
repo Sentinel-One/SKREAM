1e92cfa3121c017f833f24842c1629039ad0211b
Assaf Carlsbad <carlsbad@sentinelone.com>
2018-10-14 11:19:25 +0300

    Fix.

--- SKREAM/Config.cpp
+++ SKREAM/Config.cpp
@@ -4,4 +4,4 @@ static_assert(MIN_POOL_CHUNKS_TO_ADD >= 1,
     "MIN_POOL_CHUNKS_TO_ADD must be greater than or equal to 1");
 
 static_assert(MAX_POOL_CHUNKS_TO_ADD >= MIN_POOL_CHUNKS_TO_ADD,
-    "MIN_POOL_CHUNKS_TO_ADD must be greater than or equal to MIN_POOL_CHUNKS_TO_ADD");
+    "MAX_POOL_CHUNKS_TO_ADD must be greater than or equal to MIN_POOL_CHUNKS_TO_ADD");
